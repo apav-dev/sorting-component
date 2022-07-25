7887574f04c9bedb6e90ac3f18cb81fed6ca897f
Aaron Pavlick <apavlick@yext.com>
2022-07-25 16:00:37 -0400

    added sorting component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useAnswersActions } from "@yext/answers-headless-react";
+import {
+  useAnswersActions,
+  useAnswersState,
+} from "@yext/answers-headless-react";
 import {
   SearchBar,
   StandardCard,
   VerticalResults,
 } from "@yext/answers-react-components";
 import { useEffect } from "react";
+import SortDropdown from "./components/SortDropdown";
 
-function App() {
+function App(): JSX.Element {
   const searchActions = useAnswersActions();
 
   useEffect(() => {
@@ -17,6 +21,9 @@ function App() {
     <div className="flex justify-center px-4 py-6">
       <div className="w-full max-w-5xl">
         <SearchBar />
+        <div className="flex justify-end mb-4">
+          <SortDropdown />
+        </div>
         <VerticalResults CardComponent={StandardCard} />
       </div>
     </div>
